289720abd87ca7f59ae7499fc725ef641ff1dfe7
Tatsianacs <42011535+Tatsianacs@users.noreply.github.com>
2018-09-28 15:38:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 module.exports =
-    function longestConsecutiveLength(array) {
-        //array.sort();
-        var count = 0;
-        var max = 0;
-        var array = [];
-        for (var i = 0; i < array.length; i++) {
-            if (array[i + 1] == array[i] + 1) {
-                count++;
-            } else {
-                if (count > max) {
-                max = count;
-            }
-                count = 0;}
-        }
-        return max;
+function longestConsecutiveLength(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
     }
+    array.sort(compareNumbers);
+    var count = 1;
+    var max = 1;
+    var newarray = [];
+    //console.log(array);
+    for (var i = 0; i < (array.length -1); i++) {
+      if (array[i + 1] == array[i] + 1) {
+        count++;
+      } else {
+        if (count > max) {
+          max = count;
+        }
+          count = 1;
+      }
+    }
+    return max;
+  }
 
+  function compareNumbers(a, b) {
+    return a - b;
+  }
