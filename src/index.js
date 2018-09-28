6393774cb3f86d20dc3066dcd47591c5e1d723f5
tania <masiukevich@gmail.com>
2018-09-28 07:34:31 +0300

    implement function to find max sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+module.exports =
+    function longestConsecutiveLength(array) {
+        //array.sort();
+        var count = 0;
+        var max = 0;
+        var array = [];
+        for (var i = 0; i < array.length; i++) {
+            if (array[i + 1] == array[i] + 1) {
+                count++;
+            } else {
+                if (count > max) {
+                max = count;
+            }
+                count = 0;}
+        }
+        return max;
+    }
+
